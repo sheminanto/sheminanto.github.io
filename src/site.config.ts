137ce2efefc182f4694ce9a311626857df080bca
sheminanto <sheminanto@gmail.com>
2024-03-16 21:53:39 +0530

    Update site data

--- src/site.config.ts
+++ src/site.config.ts
@@ -5,9 +5,9 @@ export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
 	author: "Shemin",
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: "",
+	title: "Shemin",
 	// Meta property used as the default description meta property
-	description: "",
+	description: "Software Engineer | Ruby On Rails and React.js",
 	// HTML lang property, found in src/layouts/Base.astro L:18
 	lang: "en-GB",
 	// Meta property, found in src/components/BaseHead.astro L:42
