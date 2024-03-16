1f2088e2c49dc56eb1d887302e57818c699a09de
sheminanto <sheminanto@gmail.com>
2024-03-16 21:41:20 +0530

    Basic setup for home page

--- src/site.config.ts
+++ src/site.config.ts
@@ -3,11 +3,11 @@ import type { AstroExpressiveCodeOptions } from "astro-expressive-code";
 
 export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: "Chris Williams",
+	author: "Shemin",
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: "Astro Theme Cactus",
+	title: "",
 	// Meta property used as the default description meta property
-	description: "An opinionated starter theme for Astro",
+	description: "",
 	// HTML lang property, found in src/layouts/Base.astro L:18
 	lang: "en-GB",
 	// Meta property, found in src/components/BaseHead.astro L:42
@@ -21,26 +21,7 @@ export const siteConfig: SiteConfig = {
 			year: "numeric",
 		},
 	},
-	webmentions: {
-		link: "https://webmention.io/astro-cactus.chriswilliams.dev/webmention",
-	},
 };
-
-// Used to generate links in both the Header & Footer.
-export const menuLinks: Array<{ title: string; path: string }> = [
-	{
-		title: "Home",
-		path: "/",
-	},
-	{
-		title: "About",
-		path: "/about/",
-	},
-	{
-		title: "Blog",
-		path: "/posts/",
-	},
-];
 
 // https://expressive-code.com/reference/configuration/
 export const expressiveCodeOptions: AstroExpressiveCodeOptions = {
